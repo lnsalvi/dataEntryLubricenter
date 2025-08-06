d0507096b29015c66f4cd40c69c72fa05812c978
psalvi <psalvi@semapi.com.ar>
2025-08-06 16:03:50 -0300

    Initial commit
    Resolve: G#001

---
+++ js/index.js
@@ -0,0 +1,94 @@
+const BtnClear = document.getElementById('btnClear')
+const CntSales = document.getElementById('cntSales')
+const BtnAddSale = document.getElementById('btnAddSale')
+const BtnSendInformation = document.getElementById('btnSendInformation')
+
+const DateOfSale = document.getElementById('dateOfSale')
+const Product = document.getElementById('product')
+const Price = document.getElementById('price')
+const Quantity = document.getElementById('quantity')
+
+/* Functions */
+const takeData = () => {
+  let status = true
+
+  if (DateOfSale.value === '' || Product.value === '' || Price.value === '' || Quantity.value === '') status = false
+
+  if (!status) alert('Le falto cargar algun dato')
+
+  const productToSale = {
+    dateSale: DateOfSale.value,
+    nameProduct: Product.value,
+    price: Price.value,
+    quantity: Quantity.value
+  }
+
+  return {
+    productToSale,
+    status
+  }
+}
+
+function addProductToLocalStorage(productToSale) {
+  const productsInLocalStorage = localStorage.getItem('productsLubricentro')
+
+  let products = productsInLocalStorage ? JSON.parse(productsInLocalStorage) : []
+
+  products.push(productToSale)
+
+  localStorage.setItem('productsLubricentro', JSON.stringify(products))
+}
+/* Functions */
+
+
+/* Events */
+BtnClear.addEventListener('click', () => {
+  CntSales.innerHTML = ''
+  localStorage.removeItem('productsLubricentro');
+})
+
+BtnAddSale.addEventListener('click', () => {
+  const newSaleP = document.createElement('p')
+  let dataSale = takeData()
+
+  addProductToLocalStorage(dataSale.productToSale)
+
+  if (dataSale.status) {
+    newSaleP.classList.add('sale')
+
+    let data = `${dataSale.productToSale.dateSale} //// ${dataSale.productToSale.nameProduct}  --//--  ${dataSale.productToSale.price}  --//--  ${dataSale.productToSale.quantity}`
+
+    newSaleP.textContent = data
+
+    CntSales.appendChild(newSaleP)
+  }
+})
+
+BtnSendInformation.addEventListener('click', async () => {
+  const productosGuardados = localStorage.getItem('productsLubricentro')
+
+  let products =  productosGuardados ? JSON.parse(productosGuardados) : []
+
+    const Request = await fetch('http://localhost:3002/lubricentro', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(products)
+  })
+
+  const Response = await Request.json()
+
+  console.log(Response)
+})
+
+window.addEventListener('load', function() {
+  const today = new Date()
+  const year = today.getFullYear()
+  const month = String(today.getMonth() + 1).padStart(2, '0')
+  const day = String(today.getDate()).padStart(2, '0')
+
+  DateOfSale.value = `${year}-${month}-${day}`
+})
+/* Events */
